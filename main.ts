249a4e8ffd4d4984ad4f0a49db81dbbf8e6f478d
Xuefeng education <40351998+xuefengedu@users.noreply.github.com>
2019-01-24 19:47:01 +0800

    Add IntDiv and IntRemainder

--- main.ts
+++ main.ts
@@ -11,8 +11,9 @@ namespace smarttools {
      * @param input the int to be converted to string
      */
     //% blockId=smarttools_intToString block="Convert int:%input to string(removing fractions)" blockExternalInputs=false
-    //% weight=100
+    //% weight=100 blockGap=8
     //% group="Integer suite"
+    //% inlineInputMode=inline
     export function intToString(input: number): string {
         return '' + round2Zero(input);
     }
@@ -22,8 +23,9 @@ namespace smarttools {
      * @param input the string to be converted to int
      */
     //% blockId=smarttools_stringToInt block="Convert string:%input to int(removing fractions)" blockExternalInputs=false
-    //% weight=99
+    //% weight=99 blockGap=8
     //% group="Integer suite"
+    //% inlineInputMode=inline
     export function stringToInt(input: string): number {
         return round2Zero(parseInt(input));
     }
@@ -33,8 +35,9 @@ namespace smarttools {
      * @param input the decimal to be converted to int
      */
     //% blockId=smarttools_round block="Convert decimal:%input to int with round" blockExternalInputs=false
-    //% weight=90
+    //% weight=90 blockGap=8
     //% group="Integer suite"
+    //% inlineInputMode=inline
     export function round(input: number): number {
         return Math.round(input);
     }
@@ -44,8 +47,9 @@ namespace smarttools {
      * @param input the decimal to be converted to int
      */
     //% blockId=smarttools_ceil block="Convert decimal:%input to int with roundUp2PositiveInfinity" blockExternalInputs=false
-    //% weight=89
+    //% weight=89 blockGap=8
     //% group="Integer suite"
+    //% inlineInputMode=inline
     export function roundUp2PositiveInfinity(input: number): number {
         return Math.ceil(input);
     }
@@ -55,8 +59,9 @@ namespace smarttools {
      * @param input the decimal to be converted to int
      */
     //% blockId=smarttools_floor block="Convert decimal:%input to int with roundDown2NegativeInfinity" blockExternalInputs=false
-    //% weight=88
+    //% weight=88 blockGap=8
     //% group="Integer suite"
+    //% inlineInputMode=inline
     export function roundDown2NegativeInfinity(input: number): number {
         return Math.floor(input);
     }
@@ -66,8 +71,9 @@ namespace smarttools {
      * @param input the decimal to be converted to int
      */
     //% blockId=smarttools_trunc block="Convert decimal:%input to int with round2Zero" blockExternalInputs=false
-    //% weight=87
+    //% weight=87 blockGap=8
     //% group="Integer suite"
+    //% inlineInputMode=inline
     export function round2Zero(input: number): number {
         return Math.trunc(input);
     }
@@ -76,28 +82,33 @@ namespace smarttools {
      * Author.
      */
     //% blockId=smarttools_whoisauthor block="Who is author" blockExternalInputs=false
-    //% weight=80
+    //% weight=80 blockGap=8
     //% group="Author"
+    //% inlineInputMode=inline
     export function whoisauthor(): void {
         return basic.showString("github.com/51bit");
     }
 
     /**
      *  Convert hex to decimal.
+     *  @param hex_num the hex to be converted to decimal
      */
     //% blockId=smarttools_hex2Dec block="Convert hex to decimal:%hex_num" blockExternalInputs=false
-    //% weight=71
+    //% weight=71 blockGap=8
     //% group="Hex Convertor"
+    //% inlineInputMode=inline
     export function hex2Dec(hex_num: string): number {
         return parseInt(hex_num);
     }
 
     /**
      *  Convert decimal to hex.
+     *  @param dec_num the decimal to be converted to hex
      */
     //% blockId=smarttools_dec2Hex block="Convert decimal to hex:%dec_num" blockExternalInputs=false
-    //% weight=70
+    //% weight=70 blockGap=8
     //% group="Hex Convertor"
+    //% inlineInputMode=inline
     export function dec2Hex(dec_num: number): string {
         let dic: string = "0123456789ABCDEF";
         let output: string = "";
@@ -111,12 +122,38 @@ namespace smarttools {
         }
         if (len & 1) {
             output = output + "0";
-        } 
+        }
         for (len--; len >= 0; dec_num >>= 4, --len) {
             let id = dec_num & 0xf
             output = output + dic[id];
         }
 
         return "0x" + output;
+    }
+
+    /**
+     *  Div for integer.
+     *  @param a the divisor
+     *  @param b the dividend
+     */
+    //% blockId=smarttools_intDiv block="IntDiv:%a÷%b" blockExternalInputs=false
+    //% weight=61 blockGap=8
+    //% group="Integer Math"
+    //% inlineInputMode=inline
+    export function intDiv(a: number, b: number): number {
+        return Math.trunc(a / b);
+    }
+
+    /**
+     *  Remainder for integer.
+     *  @param a the divisor
+     *  @param b the dividend
+     */
+    //% blockId=smarttools_intRemainder block="IntRemainder of%a÷%b" blockExternalInputs=false
+    //% weight=60 blockGap=8
+    //% group="Integer Math"
+    //% inlineInputMode=inline
+    export function intRemainder(a: number, b: number): number {
+        return Math.trunc(a % b);
     }
 }
