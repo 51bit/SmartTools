bd82e0a08d89189061d1b8c7f96d97f3e1ad1f19
Xuefeng education <40351998+xuefengedu@users.noreply.github.com>
2019-01-25 00:10:24 -0800

    update divWithFraction

--- main.ts
+++ main.ts
@@ -214,6 +214,9 @@ namespace smarttools {
                     {
                         strb = stra.substr(0, end)
                     }
+                    for (let i = 0; i < n - (strb.length - 1 - stra.indexOf('.')); i++) {
+                        strb = strb + '0'
+                    }
                 }
             }
             else {
